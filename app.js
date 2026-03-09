cdc9e84ca7c94fde6eaa83ceb77b8da471199e3c
Second Penguin Planning <info@second-penguin.com>
2026-03-09 15:54:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ fetch("bucket-N4.json")
 kanjiList=data
 loadCard()
 })
+
 function loadCard(){
 
 let k=kanjiList[current]
@@ -19,25 +20,16 @@ document.getElementById("reading").style.display="none"
 document.getElementById("meaning").style.display="none"
 
 }
+
 function showAnswer(){
 
-document.getElementById("reading").style.display="block";
-document.getElementById("meaning").style.display="block";
+document.getElementById("reading").style.display="block"
+document.getElementById("meaning").style.display="block"
 
 }
+
 function answer(bucket){
-function loadCard(){
 
-let k=kanjiList[current]
-
-document.getElementById("kanji").innerText=k.kanji
-document.getElementById("reading").innerText="Reading: "+k.reading
-document.getElementById("meaning").innerText="Meaning: "+k.meaning
-
-document.getElementById("reading").style.display="none"
-document.getElementById("meaning").style.display="none"
-
-}
 let k=kanjiList[current]
 
 let data=JSON.parse(localStorage.getItem("progress"))||{}
